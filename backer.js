5a4dc5065a4a80dfcea27715dbbf6221664a9681
tsprograms <tsprograms@users.noreply.github.com>
2016-06-10 11:24:42 -0400

    Add outline for Model constructor

--- backer.js
+++ backer.js
@@ -3,3 +3,19 @@ tsprograms/backerJS/backer.js
 This file created on 6/10/16 by TSPrograms.
 Copyright © 2016 TSPrograms.
 */
+
+(function(global) {
+  var Model = function(initFunc) {
+    var events = {
+      
+    };
+    
+    this.properties = (typeof initFunc === 'function') ? (new initFunc()) : {};
+    this.addEvent = function(name) {
+      
+    };
+    this.callEvent = function(name, data) {
+      
+    };
+  };
+})(window);
