13441be29966f289e17c2aab7dd37122d606a7c7
tsprograms <tsprograms@users.noreply.github.com>
2016-06-12 10:05:00 -0400

    Finish event interface for Model constructor

--- backer.js
+++ backer.js
@@ -6,16 +6,32 @@ Copyright © 2016 TSPrograms.
 
 (function(global) {
   var Model = function(initFunc) {
-    var events = {
-      
-    };
+    var events = {};
     
     this.properties = (typeof initFunc === 'function') ? (new initFunc()) : {};
     this.addEvent = function(name) {
-      
+      if (!events.hasOwnProperty(name) && name !== 'hasOwnProperty') {
+        events[name] = [];
+        return true;
+      }
+      return false;
     };
     this.callEvent = function(name, data) {
-      
+      if (!events.hasOwnProperty(name)) {
+        throw 'Backer: Cannot call event "' + name + '" because it does not exist.';
+      }
+      for (var i = 0; i < events[name].length; i++) {
+        events[name][i](data);
+      }
+    };
+    this.listen = function(name, callback) {
+      if (!events.hasOwnProperty(name)) {
+        throw 'Backer: Cannot listen for event "' + name + '" because it does not exist.';
+      }
+      if (typeof callback !== 'function') {
+        throw 'Backer: Event callback must be a function.';
+      }
+      events[name].push(callback);
     };
   };
 })(window);
